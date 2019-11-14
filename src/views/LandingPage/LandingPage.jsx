4e241a8d0fe4d4ff9a37b775c9a26a271766814e
Richard Shi <shiq2@uci.edu>
2019-11-13 23:40:55 -0800

    update landing page with new design

--- src/views/LandingPage/LandingPage.jsx
+++ src/views/LandingPage/LandingPage.jsx
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 
-import 'assets/sass/views/LandingPage.scss';
+import './LandingPage.scss';
 
 import LandingAnimation from 'components/LandingAnimation/LandingAnimation';
 import PublicFooter from 'components/PublicFooter/PublicFooter';
 
 import logo from 'assets/images/logo_ever.png';
-import designPicEndLess from 'assets/images/LandingPage/endless.png'
-import designPicIDrink from 'assets/images/LandingPage/iDrink.png'
-import projectPicEatHere from 'assets/images/LandingPage/eatHere.png'
+import designPicEndLess from 'assets/images/LandingPage/Brand_P1@2x.png'
+import designPicIDrink from 'assets/images/LandingPage/Brand_P2@2x.png'
+import projectPicEatHere from 'assets/images/LandingPage/Brand_P3@2x.png'
+import designKitakata from 'assets/images/LandingPage/Brand_P4@2x.png'
+import designBigPixel from 'assets/images/LandingPage/Brand_P5@2x.png'
+import projectBabyCostReport from 'assets/images/LandingPage/Brand_P6@2x.png'
 
 class LandingPage extends Component {
     constructor(props){
@@ -38,6 +41,7 @@ class LandingPage extends Component {
                     link: "",
                 },
             ],
+            isSchoolProject: true,
         };
 
         this.setLogoAnimationCompleted = this.setLogoAnimationCompleted.bind(this);
@@ -45,6 +49,9 @@ class LandingPage extends Component {
         this.renderLogoAnimation = this.renderLogoAnimation.bind(this);
         this.renderDesignBlocks = this.renderDesignBlocks.bind(this);
         this.renderProjectBlocks = this.renderProjectBlocks.bind(this);
+        this.renderProjectsForSchoolFinal = this.renderProjectsForSchoolFinal.bind(this);
+        this.renderProjectsForJobPortfolio = this.renderProjectsForJobPortfolio.bind(this);
+
     }
 
     setLogoAnimationCompleted(animationTime){
@@ -93,10 +100,68 @@ class LandingPage extends Component {
             </div>
         ) 
     }
+    renderProjectsForSchoolFinal(){
+        let schoolDesign = [
+            {
+                name: "Kitakata",
+                id: "kitakata",
+                image: designKitakata,
+                link: "",
+            },
+            {
+                name: "BigPixel",
+                id: "bigpixel",
+                image: designBigPixel,
+                link: "",
+            },
+        ];
+        let schoolProjects = [
+            {
+                name: "BabyCostReport",
+                id: "babycostreport",
+                image: projectBabyCostReport,
+                link: "",
+            },
+        ]
+
+        let designListDisplay = schoolDesign.map((designObject, index) => {
+            return(
+                <div className="design-block">
+                    <img className="design-block__Image" src={designObject.image}></img>
+                </div>
+            );
+        })
+
+        let projectListDisplay = schoolProjects.map((projectObject, index) => {
+            return(
+                <div className="project-block">
+                    <img className="project-block__Image" src={projectObject.image}></img>
+                </div>
+            );
+        })
+        return (
+            <div>
+                <div className="LandingPage__Designs row-section">
+                    <div className="design-blocks-container">
+                        {designListDisplay}
+                    </div>
+                </div>
+
+                <div className="LandingPage__Products row-section">
+                    <div className="project-blocks-container">
+                        {projectListDisplay}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    renderProjectsForJobPortfolio(){
+        return (<div></div>)
+    }
 
 
     render(){
-        const {logoAnimationCompleted, designList, projectList} = this.state;
+        const {logoAnimationCompleted, designList, projectList, isSchoolProject} = this.state;
         
         let LandingPageClass="LandingPage__Container";
         if (logoAnimationCompleted){
@@ -106,6 +171,12 @@ class LandingPage extends Component {
         let logoAnimation = this.renderLogoAnimation();
         let designBlocks = this.renderDesignBlocks();
         let projectBlocks = this.renderProjectBlocks();
+        let dynamicProjects = {};
+        if (isSchoolProject){
+            dynamicProjects = this.renderProjectsForSchoolFinal();
+        } else {
+            dynamicProjects = this.renderProjectsForJobPortfolio();
+        }
 
 
         return(
@@ -120,18 +191,13 @@ class LandingPage extends Component {
                         </div>
                         <div className="Nav-Bar-Right">
                             <ul className="Nav-Bar-Right__Tabs">
-                                <li>Home</li>
-                                <li>Designs</li>
-                                <li>Marketing</li>
+                                <li>Works</li>
                                 <li>About Me</li>
                             </ul>
                         </div>
                     </div>
 
                     <div className="LandingPage__Instruction-Container row-section">
-                        <div className="Instruction-Left">
-                            <img className="Instruction-Left__Logo-Image" src={logo}></img>
-                        </div>
                         <div className="Instruction-Right">
                             <h1>Hi there, this is Ever Wu</h1>
                             <h2>A UX/UI, Visual Designer with a focus in user experience, visual and interaction design based in San Francisco and Irvine.</h2>
@@ -145,6 +211,8 @@ class LandingPage extends Component {
                     <div className="LandingPage__Products row-section">
                         {projectBlocks}
                     </div>
+
+                    {dynamicProjects}
 
                     <div className="LandingPage__Contact row-section">
                         <div>
